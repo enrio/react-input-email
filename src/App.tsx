6ecece6255a00f50be1e77ae325678522e794e5a
Thuc <thucnv@ichiba.vn>
2022-12-09 22:12:09 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import { useState } from 'react';
-import { InputEmailMulti } from './components/input-email';
-import './App.css';
+import { useState } from "react";
+import { InputEmailMulti } from "./components/input-email";
+
+import "./components/input-email/style.css"
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [emails, setEmail] = useState<string[]>([]);
 
   return (
     <div className="App">
       <p>aaaaa</p>
-      <InputEmailMulti />
+      <InputEmailMulti
+        placeholder="Input your email"
+        emails={emails}
+        onChange={(_emails: string[]) => {
+          setEmail(_emails);
+        }}
+        autoFocus={true}
+        getLabel={(email, index, removeEmail) => {
+          return (
+            <div data-tag key={index}>
+              <div data-tag-item>{email}</div>
+              <span data-tag-handle onClick={() => removeEmail(index)}>
+                ×
+              </span>
+            </div>
+          );
+        }}
+      />
     </div>
   );
 }
